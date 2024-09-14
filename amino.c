53d37163564de026d3996a112d548cd26012c5de
Paul Garcia <gp22@users.noreply.github.com>
2024-09-14 10:40:01 -0600

    Add getaddrinfo

--- amino.c
+++ amino.c
@@ -3,12 +3,20 @@
 #include <arpa/inet.h>
 
 int main(int argc, char *argv[]) {
-
-  int listenfd, connfd;
+  int listenfd, connfd, status;
+  struct addrinfo hints, *res;
   struct sockaddr_in servaddr;
   char buff[MAXLINE];
 
+  memset(&hints, 0, sizeof hints); // make sure the struct is empty
+  hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
+  hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+  hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+
+  status = Getaddrinfo(NULL, "3490", &hints, &res);
   listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+  freeaddrinfo(res);
 
   return EXIT_SUCCESS;
 }
