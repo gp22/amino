b28cf94fc763b960b8b9e2b91582c0dd1f161f85
Paul Garcia <gp22@users.noreply.github.com>
2024-09-15 11:21:15 -0600

    Add bind, connect

--- sockets.c
+++ sockets.c
@@ -1,6 +1,20 @@
 #include "sockets.h"
 #include "error.h"
 
+void Bind(int fd, const struct sockaddr *sa, socklen_t salen) {
+  if (bind(fd, sa, salen) < 0)
+    err_sys("bind error");
+}
+
+int Connect(int sockfd, struct sockaddr *serv_addr, int addrlen) {
+  int status;
+
+  if ((status = connect(sockfd, serv_addr, addrlen)) != 0)
+    err_sys("connect error");
+
+  return status;
+}
+
 int Getaddrinfo(char *node, char *service, struct addrinfo *hints,
                 struct addrinfo **res) {
   int status;
--- sockets.h
+++ sockets.h
@@ -2,7 +2,10 @@
 #define _commands_h
 
 #include <netdb.h>
+#include <sys/socket.h>
 
+void Bind(int, const struct sockaddr *, socklen_t);
+int Connect(int, struct sockaddr *, int);
 int Getaddrinfo(char *, char *, struct addrinfo *, struct addrinfo **);
 int Socket(int, int, int);
 
