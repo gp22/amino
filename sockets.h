612dfe4defdc6c436494923ba5a8d08f544017b1
Paul Garcia <gp22@users.noreply.github.com>
2024-09-21 08:04:16 -0600

    Add Recv

--- sockets.h
+++ sockets.h
@@ -9,6 +9,7 @@ void Bind(int, const struct sockaddr *, socklen_t);
 int Connect(int, struct sockaddr *, int);
 int Getaddrinfo(char *, char *, struct addrinfo *, struct addrinfo **);
 int Listen(int, int);
+ssize_t Recv(int, void *, size_t, int);
 int Socket(int, int, int);
 
 #endif
